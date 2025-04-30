3f75d737a314dcdbf9e19f937a08f62adb0f9bdb
Mitsu <sdkthiam4@gmail.com>
2025-04-30 11:50:38 +0000

    update: ton message

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -8,7 +8,7 @@ export const Home = () => {
     >
       <RevealOnScroll>
         <div className="text-center z-10 px-4">
-          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-[#5514e7] to-[#CB15D6] bg-clip-text text-transparent leading-right">
+          <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-[#cb15d6] to-[#6812e3] bg-clip-text text-transparent leading-right">
             Hi, I'm Mitsu
           </h1>
 
@@ -18,15 +18,15 @@ export const Home = () => {
           <div className="flex justify-center space-x-4">
             <a
               href="#projects"
-              className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
+              className="bg-[#cb15d6] text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.4)]"
             >
               View Projects
             </a>
 
             <a
               href="#contact"
-              className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 
-             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-blue-500/10"
+              className="border border-[#a425ac]/50 text-[#cb15d6] py-3 px-6 rounded font-medium transition-all duration-200 
+             hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba(59, 130, 246, 0.2)] hover:bg-[#76057d]/10"
             >
               Contact Me
             </a>
