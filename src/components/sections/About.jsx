34a7826a3cc4e8a5bb158eb9b1680b5bfa3e7783
Mitsu <sdkthiam4@gmail.com>
2025-04-30 15:12:32 +0000

    update: ton message

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -34,7 +34,7 @@ export const About = () => {
                   {frontendSkills.map((tech, key) => (
                     <span
                       key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                      className="bg-purple-500/10 text-[#8730ff] py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
                     "
                     >
@@ -65,7 +65,7 @@ export const About = () => {
                   {lowlevel.map((tech, key) => (
                     <span
                       key={key}
-                      className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
+                      className="bg-purple-500/10 text-[#8730ff] py-1 px-3 rounded-full text-sm hover:bg-purple-500/20 
                                     hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition
                     "
                     >
