8f983f5d36d73b36b1c16fea52384f3281c1a9e7
Mitsu <sdkthiam4@gmail.com>
2025-04-30 12:10:54 +0000

    update: ton message

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -17,7 +17,7 @@ export const About = () => {
     >
       <RevealOnScroll>
         <div className="max-w-3xl mx-auto px-4">
-          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center">
+          <h2 className="text-3xl font-bold mb-8 bg-gradient-to-r from-[#b823c0] to-[#cb15d6] bg-clip-text text-transparent text-center">
             {" "}
             About Me
           </h2>
